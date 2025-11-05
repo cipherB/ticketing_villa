d581a96c7581a380caf9e11dc7e311858e4f2079
cipherB <bolulawal5@gmail.com>
2025-11-05 21:29:00 +0100

    update

--- ticketing_villa/settings.py
+++ ticketing_villa/settings.py
@@ -80,25 +80,25 @@ WSGI_APPLICATION = 'ticketing_villa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'nostalgia2025$default',
-#         'USER': 'nostalgia2025',
-#         'PASSWORD': 'wordpass321',
-#         'HOST': 'nostalgia2025.mysql.pythonanywhere-services.com',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'ssl': True,  # Add this line to enable SSL
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nostalgia2025$default',
+        'USER': 'nostalgia2025',
+        'PASSWORD': 'wordpass321',
+        'HOST': 'nostalgia2025.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'ssl': True,  # Add this line to enable SSL
+        },
+    }
+}
 
 
 # Password validation
